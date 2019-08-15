09f5b8f3fa3cacdbaddfe0a85e7781b3d9a6be46
Toby Archer <me@tobbbl.es>
2019-08-15 17:27:19 +0100

    bootstrap Go http service

---
+++ server/endpoints/index/index.go
@@ -0,0 +1,26 @@
+package index
+
+import "net/http"
+
+var (
+	Path    = "/"
+	Methods = []string{"GET"}
+)
+
+// Endpoint ought to contain any dependencies used by the endpoint, such as
+// database store clients, cache clients, or loggers.
+type Endpoint struct{}
+
+func (e *Endpoint) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	response := []byte(`{"status": "ok"}`)
+
+	w.Write(response)
+}
+
+func (e *Endpoint) Path() string {
+	return Path
+}
+
+func (e *Endpoint) Methods() []string {
+	return Methods
+}
